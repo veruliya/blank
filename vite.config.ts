36debe9acdfe9695ac8982b44f192c4e29b494bc
Racsi Beryl <veruliya@proton.me>
2026-03-31 14:56:05 +0700

    disable inertia ssr

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
       input: ['resources/css/app.css', 'resources/js/app.tsx'],
       refresh: true,
     }),
-    inertia(),
+    inertia({
+      ssr: false
+    }),
     react({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
